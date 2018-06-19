23d2c43e0e3e225ca53b8938891db03fd6585239
umxe <u_habiba@rocketmail.com>
2018-06-19 14:29:04 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,5 @@
 # Build your say_hello method here
-def say_hello(name)
-  if name.eq("")
-    puts "Hello Ruby Programmer!"
-  else
-    puts "Hello #{name}!"
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
-say_hello("")
+say_hello("Ume")
